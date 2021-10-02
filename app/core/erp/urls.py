761e1ee86bac5ae2f76767b144caabe91c95e7c7
Geojorge <jorgezevallosr@gmail.com>
2021-10-01 23:15:27 -0500

    Lista basada en clase: ListView

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -1,6 +1,6 @@
 from os import name
 # from core.erp.views import myFirstView, mySecondView, prueba
-from core.erp.views.category.views import category_list
+from core.erp.views.category.views import CategoryListView, category_list
 from django.urls import path
 
 # Especifcinado nombre de las rutas
@@ -11,5 +11,6 @@ urlpatterns = [
     # path('uno/', myFirstView, name='vista1'),
     # path('dos/', mySecondView,  name='vista2'),
     # path('tres/', prueba,  name='vista3'),
-    path('category/list/', category_list, name='category_list')
+    # path('category/list/', category_list, name='category_list')
+    path('category/list/', CategoryListView.as_view(), name="category_list")
 ]
--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from core.erp.models import Category
+from core.erp.models import Category, Product
+from django.views.generic import ListView
 
 
 # Vista basada en funci´no
@@ -10,3 +11,21 @@ def category_list(request):
         'categories': Category.objects.all()
     }
     return render(request, "category/list.html", data)
+
+
+class CategoryListView(ListView):
+    #Tengo que decirle cual es el modelo
+    model = Category
+
+    #Cual es la plantilla
+    template_name = 'category/list.html'
+
+    # def get_queryset(self):
+    #     return Category.objects.filter(name__startswith = 'B')
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de categorias'
+        # context['object_list'] = Product.objects.all()
+        return context
